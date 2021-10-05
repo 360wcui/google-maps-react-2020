c5405c196aea779db3a2e35dbbf9362fc3753fef
Wei Cui <wei.cui@afit.edu>
2021-10-05 07:54:22 -0400

    finalized app.js

--- src/App.js
+++ src/App.js
@@ -9,165 +9,153 @@ import mapStyles from "./mapStyles";
 
 const libraries = ["places"];
 const mapContainerStyle = {
-  height: "100vh",
-  width: "100vw",
+    height: "100vh",
+    width: "100vw",
 };
 const options = {
-  styles: mapStyles,
-  disableDefaultUI: false,
-  zoomControl: true,
-  // mapTypeId: "satellite"
+    styles: mapStyles,
+    disableDefaultUI: false,
+    zoomControl: true,
+    mapTypeId: "satellite"
 };
 const center = {
-  lat: 38.9947278,
-  lng: -76.9491321,
+    lat: 38.9841278,
+    lng: -76.9485321,
 };
 
 export default function App() {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries,
-  });
-  const [markers, setMarkers] = React.useState([]);
-  const [selected, setSelected] = React.useState(null);
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries,
+    });
+    const [markers, setMarkers] = React.useState([]);
+    const [selected, setSelected] = React.useState(null);
 
-  const onMapClick = React.useCallback((e) => {
-    console.log(e.latLng.lat(), e.latLng.lng());
-    setMarkers((current) => [
-      ...current,
-      {
-        lat: e.latLng.lat(),
-        lng: e.latLng.lng(),
-        time: new Date(),
-      },
-    ]);
-  }, []);
+    const onMapClick = React.useCallback((e) => {
+        console.log(e.latLng.lat(), e.latLng.lng());
+        setMarkers((current) => [
+            ...current,
+            {
+                lat: e.latLng.lat(),
+                lng: e.latLng.lng(),
+                time: new Date(),
+            },
+        ]);
+    }, []);
 
-  const mapRef = React.useRef();
-  const onMapLoad = React.useCallback((map) => {
-    mapRef.current = map;
-  }, []);
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) => {
+        mapRef.current = map;
+    }, []);
 
-  const panTo = React.useCallback(({ lat, lng }) => {
-    mapRef.current.panTo({ lat, lng });
-    mapRef.current.setZoom(20);
-  }, []);
+    const panTo = React.useCallback(({lat, lng}) => {
+        mapRef.current.panTo({lat, lng});
+        mapRef.current.setZoom(20);
+    }, []);
 
-  if (loadError) return "Error";
-  if (!isLoaded) return "Loading...";
+    if (loadError) return "Error";
+    if (!isLoaded) return "Loading...";
 
-  return (
-    <div>
+    return (
+        <div>
+            <Search panTo={panTo}/>
+            <GoogleMap
+                id="map"
+                mapContainerStyle={mapContainerStyle}
+                zoom={20}
+                center={center}
+                options={options}
+                onClick={onMapClick}
+                onLoad={onMapLoad}
+            >
+                <GroundOverlay
+                    url={img1}
+                    bounds={new window.google.maps.LatLngBounds(
+                        new window.google.maps.LatLng(38.984398443116966, -76.94956330265015),
+                        new window.google.maps.LatLng(38.98505681995151, -76.94870550950368)
+                    )}
+                    onClick={onMapClick}
+                />
 
-      <Search panTo={panTo} />
-
-      <GoogleMap
-        id="map"
-        mapContainerStyle={mapContainerStyle}
-        zoom={20}
-        center={center}
-        options={options}
-        onClick={onMapClick}
-        onLoad={onMapLoad}
-      >
-        {/*lat: 38.9947278,*/}
-  {/*lng: -76.9491321,*/}
-
-      <GroundOverlay
-      // url="https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg"
-      // url="https://github.com/360wcui/google-maps-react-2020/blob/master/src/images/newark_nj_1922.jpg"
-      url={img1}
-      bounds={new window.google.maps.LatLngBounds(
-        new window.google.maps.LatLng(38.9847278, -76.9491321),
-        new window.google.maps.LatLng(38.9841278, -76.9485321)
-      )}
-      onClick={onMapClick}
-      // defaultOpacity={.5}
-    />
-
-    <GroundOverlay
-      // url="https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg"
-      // url="https://github.com/360wcui/google-maps-react-2020/blob/master/src/images/newark_nj_1922.jpg"
-      url={img2}
-      bounds={new window.google.maps.LatLngBounds(
-        new window.google.maps.LatLng(38.9841278, -76.9491321),
-        new window.google.maps.LatLng(38.9835278, -76.9485321)
-      )}
-      onClick={onMapClick}
-      // defaultOpacity={.5}
-    />
-        {markers.map((marker) => (
-          <Marker
-            key={`${marker.lat}-${marker.lng}`}
-            position={{ lat: marker.lat, lng: marker.lng }}
-            onClick={() => {
-              setSelected(marker);
-            }}
-            icon={{
-              url: `/logo192.png`,
-              origin: new window.google.maps.Point(0, 0),
-              anchor: new window.google.maps.Point(15, 15),
-              scaledSize: new window.google.maps.Size(30, 30),
-            }}
-          />
-        ))}
-
-      </GoogleMap>
-    </div>
-  );
+                <GroundOverlay
+                    // url="https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg"
+                    url={img2}
+                    bounds={new window.google.maps.LatLngBounds(
+                        new window.google.maps.LatLng(38.98375532067455, -76.94956703026241),
+                        new window.google.maps.LatLng(38.984468864794344, -76.94870415014813)
+                    )}
+                    onClick={onMapClick}
+                />
+                {markers.map((marker) => (
+                    <Marker
+                        key={`${marker.lat}-${marker.lng}`}
+                        position={{lat: marker.lat, lng: marker.lng}}
+                        onClick={() => {
+                            setSelected(marker);
+                        }}
+                        icon={{
+                            url: `/logo192.png`,
+                            origin: new window.google.maps.Point(0, 0),
+                            anchor: new window.google.maps.Point(15, 15),
+                            scaledSize: new window.google.maps.Size(30, 30),
+                        }}
+                    />
+                ))}
+            </GoogleMap>
+        </div>
+    );
 }
 
-function Search({ panTo }) {
-  const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue,
-    clearSuggestions,
-  } = usePlacesAutocomplete({
-    requestOptions: {
-      location: { lat: () => 43.6532, lng: () => -79.3832 },
-      radius: 100 * 1000,
-    },
-  });
+function Search({panTo}) {
+    const {
+        ready,
+        value,
+        suggestions: {status, data},
+        setValue,
+        clearSuggestions,
+    } = usePlacesAutocomplete({
+        requestOptions: {
+            location: {lat: () => 43.6532, lng: () => -79.3832},
+            radius: 100 * 1000,
+        },
+    });
 
-  // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+    // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+    const handleInput = (e) => {
+        setValue(e.target.value);
+    };
 
-  const handleInput = (e) => {
-    setValue(e.target.value);
-  };
+    const handleSelect = async (address) => {
+        setValue(address, false);
+        clearSuggestions();
 
-  const handleSelect = async (address) => {
-    setValue(address, false);
-    clearSuggestions();
+        try {
+            const results = await getGeocode({address});
+            const {lat, lng} = await getLatLng(results[0]);
+            panTo({lat, lng});
+        } catch (error) {
+            console.log("😱 Error: ", error);
+        }
+    };
 
-    try {
-      const results = await getGeocode({ address });
-      const { lat, lng } = await getLatLng(results[0]);
-      panTo({ lat, lng });
-    } catch (error) {
-      console.log("😱 Error: ", error);
-    }
-  };
-
-  return (
-    <div className="search">
-      <Combobox onSelect={handleSelect}>
-        <ComboboxInput
-          value={value}
-          onChange={handleInput}
-          disabled={!ready}
-          placeholder="Search your location"
-        />
-        <ComboboxPopover>
-          <ComboboxList>
-            {status === "OK" &&
-              data.map(({ id, description }) => (
-                <ComboboxOption key={id} value={description} />
-              ))}
-          </ComboboxList>
-        </ComboboxPopover>
-      </Combobox>
-    </div>
-  );
+    return (
+        <div className="search">
+            <Combobox onSelect={handleSelect}>
+                <ComboboxInput
+                    value={value}
+                    onChange={handleInput}
+                    disabled={!ready}
+                    placeholder="Search your location"
+                />
+                <ComboboxPopover>
+                    <ComboboxList>
+                        {status === "OK" &&
+                        data.map(({id, description}) => (
+                            <ComboboxOption key={id} value={description}/>
+                        ))}
+                    </ComboboxList>
+                </ComboboxPopover>
+            </Combobox>
+        </div>
+    );
 }
